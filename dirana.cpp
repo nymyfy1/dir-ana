ad5b0db24581059abd9255579fe863213dc12395
Piotr Żmuda <nmforigins@gmail.com>
2022-02-04 17:09:28 +0100

    Fixes and preparations

--- dirana.cpp
+++ dirana.cpp
@@ -6,44 +6,46 @@
 
 namespace fs = std::filesystem;
 
+void iterate(auto entry) {
+    if (entry.is_regular_file()) {
+        std::ifstream inf;
+        std::string st;
+        inf.open(entry);
+        int nb = 0;
+        while (!inf.eof()) {
+            getline(inf, st);
+            nb++;
+
+        }
+        inf.close();
+        std::cout << entry << " file" << " has " << nb << " line/s\n";
+    }
+    else if (entry.is_directory()) {
+        int nb = std::count_if(fs::directory_iterator(entry), fs::directory_iterator(), static_cast<bool(*)(const fs::path&)>(fs::is_regular_file));
+        std::cout << entry << " directory" << " has " << nb << " file/s\n";
+    }
+}
+
 void dir_iterator(std::string fpath) {
     if (!fs::exists(fpath)) {
         std::cout << "Directory not found.\n";
         return;
     }
 
-    std::string st;
+    int threadnb = std::thread::hardware_concurrency();
 
     int nb = std::count_if(fs::directory_iterator(fpath), fs::directory_iterator(), static_cast<bool(*)(const fs::path&)>(fs::is_regular_file));
-    std::cout << fpath << " edirectory" << " has " << nb << " file/s\n";
-
+    std::cout << fpath << " directory" << " has " << nb << " file/s\n";
     for (auto entry : fs::recursive_directory_iterator(fpath)) {
-        std::ifstream inf;
-        if (entry.is_regular_file()) {
-            inf.open(entry);
-            nb = 0;
-            while (!inf.eof()) {
-                getline(inf, st);
-                nb++;
-
-            }
-            inf.close();
-            std::cout << entry << " file" <<" has " << nb << " line/s\n";
-        } else if (entry.is_directory()) {
-            nb = std::count_if(fs::directory_iterator(entry), fs::directory_iterator(), static_cast<bool(*)(const fs::path&)>(fs::is_regular_file));
-            std::cout << entry << " directory" << " has " << nb << " file/s\n";
-        }
+        iterate(entry);
     }
 }
 
 int main() {
-    int threads = std::thread::hardware_concurrency(); 
     std::string fpath;
     std::cout << "Enter directory:" << "\n";
     getline(std::cin, fpath);
     dir_iterator(fpath);
-
-    //std::cout << "available threads: " << threads << std::endl;
 
     system("pause");
 
