35aeb7736675c986661dee3a4e40020e10766555
Piotr Żmuda <nmforigins@gmail.com>
2022-02-04 04:26:06 +0100

    Counting files
    The app can now count files and lines of text in the files.

--- dirana.cpp
+++ dirana.cpp
@@ -6,12 +6,19 @@
 
 namespace fs = std::filesystem;
 
-void dir_iterator(std::string path) {
-    int nb;
+void dir_iterator(std::string fpath) {
+    if (!fs::exists(fpath)) {
+        std::cout << "Directory not found.\n";
+        return;
+    }
+
     std::string st;
-    for (auto entry : fs::recursive_directory_iterator(path)) {
+
+    int nb = std::count_if(fs::directory_iterator(fpath), fs::directory_iterator(), static_cast<bool(*)(const fs::path&)>(fs::is_regular_file));
+    std::cout << fpath << " edirectory" << " has " << nb << " file/s\n";
+
+    for (auto entry : fs::recursive_directory_iterator(fpath)) {
         std::ifstream inf;
-        //std::cout << path << "\n";
         if (entry.is_regular_file()) {
             inf.open(entry);
             nb = 0;
@@ -21,18 +28,20 @@ void dir_iterator(std::string path) {
 
             }
             inf.close();
-            std::cout << entry << " entry" <<" has " << nb << " line/s\n";
+            std::cout << entry << " file" <<" has " << nb << " line/s\n";
+        } else if (entry.is_directory()) {
+            nb = std::count_if(fs::directory_iterator(entry), fs::directory_iterator(), static_cast<bool(*)(const fs::path&)>(fs::is_regular_file));
+            std::cout << entry << " directory" << " has " << nb << " file/s\n";
         }
-
     }
 }
 
 int main() {
     int threads = std::thread::hardware_concurrency(); 
-    std::string path;
+    std::string fpath;
     std::cout << "Enter directory:" << "\n";
-    getline(std::cin, path);
-    dir_iterator(path);
+    getline(std::cin, fpath);
+    dir_iterator(fpath);
 
     //std::cout << "available threads: " << threads << std::endl;
 
